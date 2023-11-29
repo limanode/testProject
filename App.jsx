7ca54b70d40212a49d98d58ce6719663083e0705
limanode <mcyc94@hotmail.com>
2023-11-29 17:36:58 +0800

    create basic app structure

--- App.jsx
+++ App.jsx
@@ -1,40 +1,15 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import {View} from 'react-native';
-import ContactListPage from './app/pages/ContactsListPage';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { strings } from './app/common/strings';
-
-
-const Stack = createNativeStackNavigator();
+import ContactStack from './app/pages/ContactStack';
 
 function App() {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="ContactList" 
-            component={ContactListPage}
-            options={{
-              headerTitle: () => {
-                <Header title={strings.contacts}/>
-              }
-            }} 
-          />
-        </Stack.Navigator>
+        <ContactStack/>
       </NavigationContainer>
     </SafeAreaProvider>
-  )
-}
-
-const Header = ({title}) => {
-  return(
-    <View style={{alignItems:'center'}}>
-      {/* <Icon name="rocket" size={30} color="#900" /> */}
-      <Text>{'test'}</Text>
-    </View>
   )
 }
 
